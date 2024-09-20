0c1c6636a789a80edd67640e978ac5bc1a64dbdb
ryleah <177492644+ryleah@users.noreply.github.com>
2024-09-20 15:06:47 -0400

    Initial creation of CSS and JS

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+
+/* function createGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement("div");
+        cell.style.height = '${600 / size}px';
+        cell.style.flexBasis = '${600 / size}px';
+        cell.classList.add('container-cell');
+        
+    }
+    container.appendChild(cell);
+}
+
+createGrid(16);
+*/
